2d07268c6ec52b6b45c9708e01a0ddf7805d1927
eric <gowest.wang@gmail.com>
2024-12-29 01:41:05 -0800

    1.add more attribute of html file for testing locator 2.add more pytest/playwright test cases

--- pytest_playwright/playwright_pytest_demo.py
+++ pytest_playwright/playwright_pytest_demo.py
@@ -24,18 +24,21 @@ def test_index_page():
 
     for each_album in all_album_list:
         page.get_by_text(str(each_album), exact=True).click()
-    #assert 2 == 2
+    assert 2 == 2
 
 
 def test_table_page():
     sum_of_pics = 12
+    init_number = 0
     page.get_by_role("link", name="TABLE").click()
     #page.get_by_role("cell", name="Spain", exact=True).click()
-    table_data = page.get_by_role("cell").count()
-    print("AAAAAAAAAAAAAAAAAAAAAA")
-    print(table_data)
-    print("AAAAAAAAAAAAAAAAAAAAAA")
+    table_rows = page.locator('tbody').locator('tr').count()
 
+    for each_row in range(int(table_rows)):
+
+        init_number = init_number + int(page.locator('tbody').locator('tr').nth(each_row).locator('td').nth(3).text_content())
+
+    assert init_number == sum_of_pics
 
 def test_new_album():
     page.get_by_role("link", name="NEW ALBUM").click()
@@ -43,3 +46,25 @@ def test_new_album():
 
 def test_upload():
     page.get_by_role("link", name="UPLOAD").click()
+
+
+def test_locator_by_id():
+    get_text = page.locator('#locator_id').inner_text()
+    assert get_text == "Locating by Id"
+
+def test_locator_by_name():
+    get_text = page.locator('[name=locator_name]').inner_text()
+    assert get_text == "Locating by Name"
+
+
+def test_locator_by_class():
+    get_text = page.locator('[class="locator_class panel panel-success panel-heading"]').inner_text()
+    assert get_text == "Locating by Class Name"
+
+def test_locator_by_xpath():
+    get_text = page.locator('xpath=/html/body/div/div/div[1]/div[4]').inner_text()
+    assert get_text == "Locating by Xpath"
+
+def test_locator_by_css():
+    get_text = page.locator('css=div[style*="background-color:#aabbcc"]').inner_text()
+    assert get_text == "Locating by CSS Selectors"
